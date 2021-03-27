b4b42502e2446efc80e4e716c9c441da41167b13
Mahalakshmi269 <mahalakshmigirajala@gmail.com>
2021-03-27 17:40:52 +0530

    initial setup: react-native-elements installed

---
+++ data.js
@@ -0,0 +1,52 @@
+export default [
+  {
+    "name": "David",
+    "profielId": "83400e947276d20b",
+    "pic": "./images/pic1.jpg"
+  },
+  {
+    "name": "Polo",
+    "profielId": "9d2c6535dc61d3b6",
+    "pic": "./images/pic2.jpg"
+  },
+  {
+    "name": "James",
+    "profielId": "066f952bb6793d2b",
+    "pic": "./images/pic3.jpg"
+  },
+  {
+    "name": "Arush",
+    "profielId": "8276cdc5acba8b49",
+    "pic": "./images/pic3.jpg"
+  },
+  {
+    "name": "Ariv",
+    "profielId": "223bb8ebe75df8d0",
+    "pic": "./images/pic1.jpg"
+  },
+  {
+    "name": "Ayansh",
+    "profielId": "0cd0c0cc57b4b36b",
+    "pic": "./images/pic2.jpg"
+  },
+  {
+    "name": "Akash",
+    "profielId": "08cff1b7bea99649",
+    "pic": "./images/pic3.jpg"
+  },
+  {
+    "name": "Sravan",
+    "profielId": "8560c47cdd63ea70",
+    "pic": "./images/pic1.jpg"
+  },
+  {
+    "name": "Fedric",
+    "profielId": "6df93622f7f5fb61",
+    "pic": "./images/pic2.jpg"
+  },
+  {
+    "name": "Pranav",
+    "profielId": "03f24a020b744ac6",
+    "pic": "./images/pic3.jpg"
+  }
+]
