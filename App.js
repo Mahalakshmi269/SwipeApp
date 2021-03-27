adfaa93807c250c76f0f4d05f1fba1471baa1200
Mahalakshmi269 <mahalakshmigirajala@gmail.com>
2021-03-27 18:15:15 +0530

    Basic Card Profile display functionality added

--- App.js
+++ App.js
@@ -1,41 +1,93 @@
- import React, {Component} from 'react';  
-import {Platform, StyleSheet, Text, View} from 'react-native';  
-  
-const instructions = Platform.select({  
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',  
-  android:  
-    'Double tap R on your keyboard to reload,\n' +  
-    'Shake or press menu button for dev menu',  
-});  
-  
-type Props = {};  
-export default class App extends Component<Props> {  
-  render() {  
-    return (  
-      <View style={styles.container}>  
-        <Text style={styles.welcome}>Welcome to React Native!</Text>  
-        <Text style={styles.instructions}>To get started, edit App.js</Text>  
-        <Text style={styles.instructions}>{instructions}</Text>  
-      </View>  
-    );  
-  }  
-}  
-  
-const styles = StyleSheet.create({  
-  container: {  
-    flex: 1,  
-    justifyContent: 'center',  
-    alignItems: 'center',  
-    backgroundColor: '#F5FCFF',  
-  },  
-  welcome: {  
-    fontSize: 20,  
-    textAlign: 'center',  
-    margin: 10,  
-  },  
-  instructions: {  
-    textAlign: 'center',  
-    color: '#333333',  
-    marginBottom: 5,  
-  },  
+import React, { Component } from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+  StyleSheet,
+  Platform,
+  Image
+} from 'react-native';
+import { Card, Button, Icon } from 'react-native-elements';
+import profiles from './data';
+
+class App extends Component {
+  state = {
+    leftClicks: 0,
+    rightClicks: 0,
+    displayTextStatus: false
+  };
+
+  handleLeftClick = () => {
+    this.setState(({ leftClicks }) => ({
+      leftClicks: leftClicks + 1
+    }));
+  };
+
+  handleRightClick = () => {
+    this.setState(({ rightClicks }) => ({
+      rightClicks: rightClicks + 1
+    }));
+  };
+
+  renderProfiles(profile) {
+    return (
+      <Card title={profile.name} titleStyle={{ fontSize: 14 }}>
+        <View style={{ height: 200 }}>
+          <Image
+            source={require(profile.pic)}
+            style={{ width: '100%', height: 200 }}
+          />
+        </View>
+      </Card>
+    );
+  }
+
+  renderNoMoreProfiles = () => {
+    return (
+      <Card title="No More Profiles">
+        <Button
+          title="Done"
+          large
+          backgroundColor="#03A9F4"
+          onPress={onDonePress}
+        />
+      </Card>
+    );
+  };
+
+  onDonePress = () => {
+    this.setState(({ displayTextStatus }) => ({
+      displayTextStatus: true
+    }));
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        this.state.displayTextStatus ? <View style={styles.statusStyle}>
+          <Text style={styles.displayTextStyle}>Right Click(s): {this.state.rightClicks}</Text>
+          <Text style={styles.displayTextStyle}>Left Click(s): {this.state.leftClicks}</Text>
+        </View> : null        
+        {this.renderProfiles}
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  statusStyle: {
+    padding: 15,
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  displayTextStyle: {
+    fontSize: 20,
+    color: 'blue'
+  }
 });
+
+export default App;
